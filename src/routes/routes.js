d30a218b4f969f141a07bf67e6006bdd74a7a7b2
Josaidcnt <josai.dcnt@gmail.com>
2025-09-13 20:46:59 -0600

    :hammer: deleted routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,20 +5,4 @@ const router = Router();
 
 router.get('/v1/restaurant/:restaurantId', getRestaurantController);
 
-router.post('/v1/restaurant', (req, res) => {
-	req.body;
-	res.send('Create restaurant');
-});
-
-router.put('/v1/restaurant/:restaurantId', (req, res) => {
-	const { restaurantId } = req.params;
-	req.body;
-	res.send('Update restaurant');
-});
-
-router.delete('/v1/restaurant/:restaurantId', (req, res) => {
-	const { restaurantId } = req.params;
-	res.send('Delete restaurant');
-});
-
 export default router;
